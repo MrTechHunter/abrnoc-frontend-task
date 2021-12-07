44b68b92f9a3ed2857639c6ab3349f540333f8f8
MrTechHunter <this.is.techhunter@gmail.com>
2021-12-08 02:06:30 +0330

    Account and Dashboard components added and working properly  + vue-router added to dependencies

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app>
+    <v-toolbar color="primary" dark fixed app>
+      <v-toolbar-title>Dashboard</v-toolbar-title>
+    </v-toolbar>
+    <v-navigation-drawer permanent>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">
+              {{ email || "Email" }}
+            </v-list-item-title>
+            <v-list-item-subtitle v-on:click="logout"
+              >log out</v-list-item-subtitle
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Servers", icon: "mdi-server" },
+        { title: "About", icon: "mdi-help-box" },
+      ],
+      email: this.$route.params.email,
+    };
+  },
+  methods: {
+    logout() {
+      this.$router.replace({ name: "account" });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
